d64d9ce03988cfc664c29eb1d22259efb7bfb439
aneri <aneriavala26@gmail.com>
2024-05-24 12:33:13 +0530

    fix page reload issue

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -7,21 +7,38 @@ export const metadata = {
     description: 'Next.js Authentication',
 };
 
-// export default function AuthLayout({ children }) {
-export default function AuthRootLayout({ children }) {
+export default function AuthLayout({ children }) {
+
     return (
-        <html lang="en">
-            <body>
-                <header id='auth-header'>
-                    <p>Welcome Back!</p>
+        <>
+            <header id='auth-header'>
+                <p>Welcome Back!</p>
 
-                    <form action={logout}>
-                        <button>Logout</button>
-                    </form>
-                </header>
+                <form action={logout}>
+                    <button>Logout</button>
+                </form>
+            </header>
 
-                {children}
-            </body>
-        </html>
+            {children}
+        </>
     );
 }
+
+
+// export default function AuthRootLayout({ children }) {
+//     return (
+//         <html lang="en">
+//             <body>
+//                 <header id='auth-header'>
+//                     <p>Welcome Back!</p>
+
+//                     <form action={logout}>
+//                         <button>Logout</button>
+//                     </form>
+//                 </header>
+
+//                 {children}
+//             </body>
+//         </html>
+//     );
+// }
